1d95bd620a506b153f9a6b90ead3edf84c149b63
Nikhil <nickchoudharyc72@gmail.com>
2022-04-11 16:42:18 +0530

    Added new character via post

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WEB_API.Models;
 
@@ -7,12 +9,31 @@ namespace WEB_API.Controllers
     [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character Knight = new Character();
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character {Id=1,Name ="Sam"}
 
-        [HttpGet]
-        public ActionResult<Character> Get()
+        };
+
+
+        //[HttpGet]
+        //[Route("GetAll")]
+        [HttpGet("GetAll")]
+        public ActionResult <List<Character>> Get()
         {
-        return Ok(Knight);
+        return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <Character> GetSingle(int id){
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+
+        public ActionResult <List<Character>> AddCharacter(Character newCharacter){
+            characters.Add(newCharacter);
+            return Ok(characters);
         }
     }
 
